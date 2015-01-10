380cc0db8e34540ffb82901f771ac0162def11f7
Prashant Khanduri <pkhanduri@jawbone.com>
2015-01-10 13:08:44 -0800

    Adding _scripts for cleaning up repo

---
+++ _scripts/clean.sh
@@ -0,0 +1,11 @@
+rm -rf _posts/*
+rm -rf _draft/*
+rm -rf assets/posts/*
+rmdir assets/posts/
+rm -rf projects/*
+rmdir projects/
+rm -rf .git/*
+rmdir .git/
+git init
+git add . 
+git commit -m "initial commit"
